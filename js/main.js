149fa4398a0d51fdc8473d82c2d732abe665e62f
Alexander Kallaway <kallaway@me.com>
2016-06-23 01:13:32 -0400

    setting things up for the project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// TODO: Modes: All, Online, Offline
+// TODO: Send request to TwitchTV API
+// TODO: Ask Annie to help choose the color scheme and think about the design together
+
+
+// Example Array of Names: ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404"]
+
+
+let streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404"];
